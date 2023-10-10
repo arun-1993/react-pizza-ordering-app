999fd68a816388cba8b2aad8a89227a784d20ac2
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-10-10 22:12:55 +0530

    feat: added an error element to handle errors when fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
+import AppLayout from "./ui/AppLayout";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
     {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
                 path: "/menu",
                 element: <Menu />,
                 loader: menuLoader,
+                errorElement: <Error />,
             },
             {
                 path: "/cart",
