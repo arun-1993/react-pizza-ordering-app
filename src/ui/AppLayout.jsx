8cc0cf62c980e162af9b233cea2774d3c4cd6347
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-10-10 21:58:28 +0530

    feat: added a loading indicator
    - Added a Loader component that shows up on a page if data fetching has not been completed
    - Modified styling for AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
-import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+
+import Loader from "./Loader";
 import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
 
 export default function AppLayout() {
+    const navigation = useNavigation();
+    const isLoading = navigation.state === "loading";
+
     return (
-        <div>
+        <div className="layout">
+            {isLoading && <Loader />}
             <Header />
 
             <main>
