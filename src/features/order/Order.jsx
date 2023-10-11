60849b08b36bc80a0951394916204f188dc146f6
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-10-11 09:24:46 +0530

    feat: added a functionality to search for an order by id in the page header

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,45 +1,14 @@
+import { useLoaderData } from "react-router-dom";
+
+import { getOrder } from "../../services/apiRestaurant";
 import {
     calculateMinutesLeft,
     formatCurrency,
     formatDate,
 } from "../../utils/helpers";
 
-const order = {
-    id: "ABCDEF",
-    customer: "Arun",
-    phone: "123456789",
-    address: "Arroios, Lisbon , Portugal",
-    priority: true,
-    estimatedDelivery: "2027-04-25T10:00:00",
-    cart: [
-        {
-            pizzaId: 7,
-            name: "Napoli",
-            quantity: 3,
-            unitPrice: 16,
-            totalPrice: 48,
-        },
-        {
-            pizzaId: 5,
-            name: "Diavola",
-            quantity: 2,
-            unitPrice: 16,
-            totalPrice: 32,
-        },
-        {
-            pizzaId: 3,
-            name: "Romana",
-            quantity: 1,
-            unitPrice: 15,
-            totalPrice: 15,
-        },
-    ],
-    position: "-9.000,38.000",
-    orderPrice: 95,
-    priorityPrice: 19,
-};
-
-function Order() {
+export default function Order() {
+    const order = useLoaderData();
     const { status, priority, priorityPrice, orderPrice, estimatedDelivery } =
         order;
     const deliveryIn = calculateMinutesLeft(estimatedDelivery);
@@ -50,7 +19,7 @@ function Order() {
                 <h2>Status</h2>
 
                 <div>
-                    {priority && <span>Priority</span>}
+                    {priority && <span>Priority </span>}
                     <span>{status} order</span>
                 </div>
             </div>
@@ -80,4 +49,7 @@ function Order() {
     );
 }
 
-export default Order;
+export async function loader({ params }) {
+    const order = await getOrder(params.orderId);
+    return order;
+}
