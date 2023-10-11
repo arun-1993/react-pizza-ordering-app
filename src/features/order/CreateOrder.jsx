c132b53d88569ca3cfd18a69e30a84a7dd92bb55
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2023-10-11 11:46:40 +0530

    feat: added functionality to create a new order
    - Used the route actions functionality of react router for form submission and error handling
    - Redirects to the orders page with your order id in case of a successful order

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,9 +1,47 @@
-function CreateOrder() {
+import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
+
+import { createOrder } from "../../services/apiRestaurant";
+
+const isValidPhone = (str) =>
+    /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
+        str
+    );
+
+const fakeCart = [
+    {
+        pizzaId: 12,
+        name: "Mediterranean",
+        quantity: 2,
+        unitPrice: 16,
+        totalPrice: 32,
+    },
+    {
+        pizzaId: 6,
+        name: "Vegetale",
+        quantity: 1,
+        unitPrice: 13,
+        totalPrice: 13,
+    },
+    {
+        pizzaId: 11,
+        name: "Spinach and Mushroom",
+        quantity: 1,
+        unitPrice: 15,
+        totalPrice: 15,
+    },
+];
+
+export default function CreateOrder() {
+    const formErrors = useActionData();
+    const navigation = useNavigation();
+    const isSubmitting = navigation.state === "submitting";
+    const cart = fakeCart;
+
     return (
         <div>
             <h2>Ready to order? Let's go!</h2>
 
-            <form>
+            <Form method="POST">
                 <div>
                     <label>First Name</label>
                     <input type="text" name="customer" required />
@@ -14,6 +52,7 @@ function CreateOrder() {
                     <div>
                         <input type="tel" name="phone" required />
                     </div>
+                    {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
 
                 <div>
@@ -31,11 +70,36 @@ function CreateOrder() {
                 </div>
 
                 <div>
-                    <button>Order now</button>
+                    <input
+                        type="hidden"
+                        name="cart"
+                        value={JSON.stringify(cart)}
+                    />
+                    <button disabled={isSubmitting}>
+                        {isSubmitting ? "Placing order..." : "Order now"}
+                    </button>
                 </div>
-            </form>
+            </Form>
         </div>
     );
 }
 
-export default CreateOrder;
+export async function action({ request }) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+    const errors = {};
+    const order = {
+        ...data,
+        cart: JSON.parse(data.cart),
+        priority: data.priority === "on",
+    };
+
+    if (!isValidPhone(order.phone))
+        errors.phone = "Please enter a valid phone number";
+
+    if (Object.keys(errors).length > 0) return errors;
+
+    const newOrder = await createOrder(order);
+
+    return redirect(`/order/${newOrder.id}`);
+}
